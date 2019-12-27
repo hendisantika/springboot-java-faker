de4ad0b67c57407b707e31e9322ba0ee969ba443
Hendi Santika <hendisantika@gmail.com>
2019-12-28 05:39:10 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootjavafaker/SpringbootJavaFakerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjavafaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaFakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJavaFakerApplication.class, args);
+    }
+
+}
