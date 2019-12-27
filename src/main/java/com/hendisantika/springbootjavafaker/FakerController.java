163022e9b8ce52c5ef35c760e341a79f1c2c3da2
Hendi Santika <hendisantika@gmail.com>
2019-12-28 05:53:35 +0700

    #6 - Add FakerController

---
+++ src/main/java/com/hendisantika/springbootjavafaker/FakerController.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springbootjavafaker;
+
+import com.github.javafaker.Faker;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+import static com.hendisantika.springbootjavafaker.FakerUtil.getFakerObjects;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-java-faker
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/12/19
+ * Time: 05.52
+ */
+@Controller
+public class FakerController {
+    @GetMapping("/")
+    public String index(@RequestParam(defaultValue = "US") String locale, Model model) {
+        model.addAttribute("locales", Arrays.asList(Locale.US.getCountry(),
+                Locale.FRANCE.getCountry(),
+                Locale.GERMANY.getCountry()));
+
+        Faker faker = new Faker(new Locale(locale));
+        model.addAttribute("values", getFakerObjects(faker));
+        return "faker.html";
+    }
+}
